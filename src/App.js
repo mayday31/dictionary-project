f2621f6707eda59a5b5f2d92c0bb33b51930c7de
mayday31 <brianajlynn@gmail.com>
2026-01-30 00:09:57 -0500

    Set up the project, adding react and bootstrap. Included logo

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import bee from "./bee.png";
+import "./App.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={bee} className="App-logo img-fluid" alt="logo" />
+      </header>
+    </div>
+  );
+}
