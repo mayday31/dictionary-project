09cfb205d61fb925b77d45472ef3c3de803a2b18
mayday31 <brianajlynn@gmail.com>
2026-02-01 13:34:47 -0500

    Created meaning and output components for the dictionary

--- src/Output.js
+++ src/Output.js
@@ -1,4 +1,5 @@
 import React from "react";
+import Meaning from "./Meaning";
 
 export default function Output(props) {
   console.log(props.output);
@@ -6,6 +7,13 @@ export default function Output(props) {
     return (
       <div className="Output">
         <h2>{props.output.word}</h2>
+        {props.output.map(function (meaning, index) {
+          return (
+            <div key={index}>
+              <Meaning meaning={meaning} />
+            </div>
+          );
+        })}
       </div>
     );
   } else {
